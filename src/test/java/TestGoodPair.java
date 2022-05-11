349981822f9f64e31f606abcd33a46b92303e62f
Shemet Bohdan <shemet.bohdan01@gmail.com>
2022-05-11 20:01:58 +0300

    fix homework

--- src/test/java/TestGoodPair.java
+++ src/test/java/TestGoodPair.java
@@ -9,22 +9,9 @@ import java.io.PrintStream;
 
 public class TestGoodPair {
 
-    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-
-    @Before
-    public void setOut(){
-        System.setOut(new PrintStream(outputStream));
-    }
-
     @Test
     public void calcGoodPair(){
         GoodPair goodPair = new GoodPair();
         Assert.assertEquals(6,goodPair.calcGoodPair(new int[]{1, 1, 1, 1}));
     }
-
-    @After
-    public void cleanUp(){
-        System.setOut(null);
-    }
-
 }
--- src/test/java/TestJewels.java
+++ src/test/java/TestJewels.java
@@ -9,22 +9,10 @@ import java.io.PrintStream;
 
 public class TestJewels {
 
-    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-
-    @Before
-    public void setOut(){
-        System.setOut(new PrintStream(outputStream));
-    }
-
     @Test
     public void testCalcQuantityJewels(){
 
         Jewels jewels = new Jewels();
         Assert.assertEquals(3,jewels.calcQuantityJewels("aA","aAAbbbb"));
-    }
-
-    @After
-    public void cleanUp(){
-        System.setOut(null);
     }
 }
